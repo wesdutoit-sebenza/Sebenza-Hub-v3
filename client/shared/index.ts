98ff4a13830754454251196495bee7b865437648
wesdutoit <48888368-wesdutoit@users.noreply.replit.com>
2025-11-27 06:22:39 +0000

    Restored to 'a1cbf27da6b09b5080a733d96cf46993ab8deba5'
    Replit-Restored-To: a1cbf27da6b09b5080a733d96cf46993ab8deba5

--- client/shared/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './cities';
-export * from './countries';
-export * from './countryCodes';
-export * from './jobTaxonomies';
-export * from './jobTitles';
-export * from './skills';
-export * from './skillsMigration';
