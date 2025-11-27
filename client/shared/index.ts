67020215ef638d774f3516fb0f475919004819be
wesdutoit <48888368-wesdutoit@users.noreply.replit.com>
2025-11-27 06:21:52 +0000

    Restored to '45d6edec3ae77d4fcc08fa9e4d945becdb958da7'
    Replit-Restored-To: 45d6edec3ae77d4fcc08fa9e4d945becdb958da7

---
+++ client/shared/index.ts
@@ -0,0 +1,7 @@
+export * from './cities';
+export * from './countries';
+export * from './countryCodes';
+export * from './jobTaxonomies';
+export * from './jobTitles';
+export * from './skills';
+export * from './skillsMigration';
